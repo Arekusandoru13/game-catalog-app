82a974a8e15f89f00df71fcd9476d33488b37ab6
Arekusandoru13 <arekusandoru@derpy.ru>
2026-01-14 22:43:30 +0300

    get_game_cli method added

--- run.py
+++ run.py
@@ -17,6 +17,17 @@ def add_game_cli(catalog):
         print(f"Такая игра уже существует с ID {add_result[1]}")
 
 
+def get_game_cli(catalog):
+    id = input("Введите ID игры: ")
+    try:
+        game_data = catalog.get_game(id)
+    except Exception as e:
+        print(e)
+        return
+    for k, v in game_data.items():
+        print(f"{k.title()}: {v}")
+    
+
 def main():
     print("Мой каталог игр.")
     catalog = GameCatalog()
@@ -24,7 +35,7 @@ def main():
     while True:
         print("\nВыберите действие:\n"
               "1. Добавить игру в каталог.\n"
-              "2. Просмотреть игру.\n"
+              "2. Просмотреть игру по ID.\n"
               "3. Обновить данные игры.\n"
               "4. Удалить игру из каталога.\n"
               "5. Отобразить весь каталог.\n"
@@ -33,6 +44,7 @@ def main():
 
         match selector:
             case "1": add_game_cli(catalog)
+            case "2": get_game_cli(catalog)
             case "0": break
             case _: print("Неподдерживаемый ввод."); continue
 
