7b14b5b0c5433279229c62d08653ebc732e0fed4
Arekusandoru13 <arekusandoru@derpy.ru>
2026-01-14 22:28:10 +0300

    add_game_cli method added

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-import game_catalog as g_cat
+from game_catalog import *
 
-def add_game_cli():
+def add_game_cli(catalog):
     print("Введите данные игры: ")
     title = input("Название: ")
     platform = input("Платформа: ")
@@ -10,16 +10,19 @@ def add_game_cli():
     genres = genres_str.split(", ")
     status = input("Статус добавленной игры: ")
     comment = input("Комментарий: ")
-
-
+    add_result = catalog.add_game(title, platform, year, genres, status, comment)
+    if add_result[0]:
+        print(f"Игра успешно добавлена с ID {add_result[1]}")
+    else:
+        print(f"Такая игра уже существует с ID {add_result[1]}")
 
 
 def main():
     print("Мой каталог игр.")
-    catalog = g_cat.GameCatalog()
+    catalog = GameCatalog()
 
     while True:
-        print("Выберите действие:\n"
+        print("\nВыберите действие:\n"
               "1. Добавить игру в каталог.\n"
               "2. Просмотреть игру.\n"
               "3. Обновить данные игры.\n"
@@ -29,7 +32,7 @@ def main():
         selector = input("Ваш выбор: ")
 
         match selector:
-            case "1": add_game_cli()
+            case "1": add_game_cli(catalog)
             case "0": break
             case _: print("Неподдерживаемый ввод."); continue
 
