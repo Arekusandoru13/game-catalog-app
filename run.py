ae3441c1bf94ecae280f7dde745f6f73b34ba70c
Arekusandoru13 <arekusandoru@derpy.ru>
2026-01-16 15:53:34 +0300

    show_all_games method added

--- run.py
+++ run.py
@@ -94,7 +94,12 @@ def delete_game_cli(catalog):
     print(f"Игра {game_data.title} удалена из каталога.\n")
 
 def show_all_games(catalog):
-    print(catalog.game_catalog)
+    if not catalog:
+        print("Список пуст.")
+        return
+    print("\nВсе игры в каталоге:")
+    for game_id, game in catalog.game_catalog.items():
+        print(f"{game_id:>15} - {game.title}")
 
 
 def main():
